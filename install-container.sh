c29a017f1db522a20b647d458c002e63b0083c5d
Joao Pedro Afonso Cerqueira <jpacerqueira@gmail.com>
2020-02-25 15:57:34 +0000

    Update install-container.sh

--- install-container.sh
+++ install-container.sh
@@ -3,7 +3,7 @@
 DOCKERLS=$(docker ps | grep lost_saturn | awk -F' ' '{ print $15 }')
 echo "$DOCKERLS"
 #
-if [ $DOCKERLS != "lost_saturn" ]; then
+if [[ $DOCKERLS != "lost_saturn" ]]; then
    docker run  -itd -p 9003:9003 -p 54321:54321 --cap-add=NET_ADMIN --name lost_saturn  jpacerqueira83/datascience-fullstack-vm1:latest
    echo "  wait 6 min "
    sleep 480
@@ -12,7 +12,7 @@ fi
 HERES=$(pwd)
 FILES=Setup-System-Java-Spark-h2o.ai-pyarrow.ipynb # ipynb in test to load addicional libraries
 #
-if [ -f "$HERES/notebooks/$FILES" ]; then
+if [[ -f "$HERES/notebooks/$FILES" ]]; then
    ARGS1="nbconvert --to notebook --execute --allow-errors --ExecutePreporcessor.timeout=480 notebooks/$FILES "
    docker exec -it lost_saturn /bin/bash -c "cd ; source .bashrc ; bash -x ~/anaconda3/bin/jupyter $ARGS1 "
    docker exec -it lost_saturn /bin/bash -c "cd ; source .bashrc ; bash -x start-jupyter.sh ; sleep 4 ; cat notebooks/jupyter.log ; sleep infinity"
