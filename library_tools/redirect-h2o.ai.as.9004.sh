c7e97846f62b78abe39c6f501da54826dc735192
jpacerqueira <jpacerqueira@gmail.com>
2020-02-27 11:13:07 +0000

    redirect traffic cares review

--- library_tools/redirect-h2o.ai.as.9004.sh
+++ library_tools/redirect-h2o.ai.as.9004.sh
@@ -11,8 +11,11 @@ sudo systemctl status ssh
 sudo echo '# try_files $uri $uri/ =404;' >> /etc/nginx/sites-available/default
 sudo echo 'proxy_pass http://127.0.0.1:54321;' >> /etc/nginx/sites-available/default
 #
-echo '/home/joci/.ssh/thekey.pem' | ssh-keygen
-chmod 400 /home/joci/.ssh/thekey.pem
+mkdir -p /home/notebookuser/.ssh/
+sudo rm -f /home/notebookuser/.ssh/thekey.pem 
+touch /home/notebookuser/.ssh/thekey.pem
+echo '/home/notebookuser/.ssh/thekey.pem' | ssh-keygen
+chmod 400 /home/notebookuser/.ssh/thekey.pem
 sudo service nginx restart
-ssh -i /home/notebookuser/.ssh/thekey.pem -R 9004:localhost:54321 notebookuser@$(hostname -I | cut -d' ' -f1)
+ssh -i /home/notebookuser/.ssh/thekey.pem -R 9004:127.0.0.1:54321 -p 9004 notebookuser@127.0.0.1 #$(hostname -I | cut -d' ' -f1)
 ########
