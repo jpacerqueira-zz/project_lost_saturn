77a424056b62e1b3703eb64929547fcee26ebe10
Joao Pedro Afonso Cerqueira <jpacerqueira@gmail.com>
2020-03-06 10:31:00 +0000

    Update use-delta-lake.sh
    Setup use latest delta-lake 0.5 delta.io

--- library_tools/use-delta-lake.sh
+++ library_tools/use-delta-lake.sh
@@ -1,3 +1,4 @@
-
-echo '(USE THIS)$ pyspark --packages io.delta:delta-core_2.11:0.3.0 '
-echo '(OR USE )$  bin/spark-shell --packages io.delta:delta-core_2.11:0.3.0 ' 
+#/bin/bash env
+echo '(USE THIS)$ pyspark --packages io.delta:delta-core_2.11:0.5.0 '
+echo '(OR USE )$  bin/spark-shell --packages io.delta:delta-core_2.11:0.5.0 ' 
+#
