e1e5e62a10ad0078c81e42a494b65e3ca7e49c3f
jpacerqueira <jpacerqueira@gmail.com>
2020-04-15 00:41:37 +0100

    Resolved rpy2==3.2.7 version and redr R library issues version 1.2.1 in R 3.5.1 required in Ubunto machines

--- library_tools/setup_install_conda_r.sh
+++ library_tools/setup_install_conda_r.sh
@@ -2,6 +2,8 @@
 echo -e "## Default repo\nlocal({r <- getOption(\"repos\")\n       r[\"CRAN\"] <- \"http://cran.r-project.org\"\n       options(repos=r)\n})\nSys.setenv(TZ=\"GMT\") " > $HOME/.Rprofile
 conda install -y r-base r-caret
 pip install rpy2==3.2.7
+pip install pyweatherbit==2.0.0
+pip install gmplot==1.2.0
 #
-echo -e " install.packages(\"readr\",repos=\"http://cloud.r-project.org/\", type=\"source\") \n library(readr) \n sessionInfo() " > $HOME/R_install_packages.R
+echo -e " install.packages(\"readr\",repos=\"http://cloud.r-project.org/\", type=\"source\") \n install.packages(\"pracma\") \n install.packages(\"Metrics\") \n install.packages(\"reshape\") \n library(readr) \n sessionInfo() " > $HOME/R_install_packages.R
 R -f $HOME/R_install_packages.R
