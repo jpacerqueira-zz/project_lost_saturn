5d4041c5f177f50486521716095fd6fd53f1a907
jpacerqueira <jpacerqueira@gmail.com>
2020-04-14 23:57:32 +0100

    Resolved rpy2==3.2.7 version and redr R library issues version 1.2.1 in R 3.5.1 required in Ubunto machines

--- library_tools/setup_install_conda_r.sh
+++ library_tools/setup_install_conda_r.sh
@@ -1,5 +1,7 @@
 #\env\bash
 echo -e "## Default repo\nlocal({r <- getOption(\"repos\")\n       r[\"CRAN\"] <- \"http://cran.r-project.org\"\n       options(repos=r)\n})\nSys.setenv(TZ=\"GMT\") " > $HOME/.Rprofile
-conda install -y r-base r-caret
-pip install rpy2
+conda install -y r-base r-base-dev r-caret
+pip install rpy2==3.2.7
 #
+echo -e " install.packages(\"readr\",repos=\"http://cloud.r-project.org/\", type=\"source\") \n library(readr) \n sessionInfo() " > $HOME/R_install_packages.R
+R -f $HOME/R_install_packages.R
