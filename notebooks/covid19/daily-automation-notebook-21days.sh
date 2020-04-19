5097431c77c36405d09501e6ed000af6d7c0e476
jpacerqueira <jpacerqueira@gmail.com>
2020-04-19 15:13:51 +0100

    #COVID19 - daily Automation bashscript for - ccrontab -e

--- notebooks/covid19/daily-automation-notebook-21days.sh
+++ notebooks/covid19/daily-automation-notebook-21days.sh
@@ -3,8 +3,8 @@
 #cd $HOME
 DATENB=$(date +'%Y-%m-%d')
 #
-$HOME/anaconda3/bin/python \
-                      --execute \
-                      --to notebook $HOME/notebooks/covid19/MY_COVID19-Prediction_00MMYYYY.ipynb \
-                      --output $HOME/notebooks/covid19/MY_COVID19-Prediction_${DATENB}-output-candidate.ipynb
+jupyter nbconvert --to notebook --execute --allow-errors --ExecutePreprocessor.timeout=180
+$HOME/anaconda3/bin/jupyter\
+          	      nbconvert --to notebook --execute --allow-errors --ExecutePreprocessor.timeout=180 \
+		      $HOME/notebooks/covid19/MY_COVID19-Prediction_00MMYYYY.ipynb  --output $HOME/notebooks/covid19/MY_COVID19-Prediction_${DATENB}-output-candidate.ipynb
 #
