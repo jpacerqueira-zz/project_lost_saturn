7d4eff98db9700ccfa78cc48f6867127887167b3
jpacerqueira <jpacerqueira@gmail.com>
2020-05-14 10:39:44 +0100

    Updated install-container.sh

--- install-container.sh
+++ install-container.sh
@@ -7,8 +7,16 @@ FILES=Setup-System-Java-Spark-h2o.ai-pyarrow.ipynb # ipynb in test to load addic
 DEST_FOLDER=/home/notebookuser/notebooks/
 #
 if [[ $DOCKERLS != "lost_saturn" ]] && [[ -f "$HERES/notebooks/$FILES" ]]; then
-#   docker run  -itd -p 9003:9003 -p 54321:54321 --cap-add=NET_ADMIN --name lost_saturn  jpacerqueira83/datascience-fullstack-vm1:latest
+   echo "setup ; option1"
    docker run  -itd -p 9003:9003   --cap-add=NET_ADMIN --name lost_saturn --mount type=bind,source="$(pwd)"/notebooks,target=${DEST_FOLDER}  jpacerqueira83/datascience-fullstack-vm1:latest
+   echo "  wait 12 min  - lost_saturn - full setup in progress - STEP1"
+   sleep 720
+   docker exec -it lost_saturn /bin/bash -c "cd ; source .bashrc ; bash -x start-jupyter.sh ; bash -x stop-jupyter.sh ; sleep 4 ; echo 'setup1: smoke test' "
+   docker exec -it lost_saturn /bin/bash -c "cd ; source .bashrc ; bash -x install-jupyter-support-packs.sh ; sleep 4 ; echo 'setup2 : additional libraries instalation' "
+   docker exec -it lost_saturn /bin/bash -c "cd ; source .bashrc ; bash -x start-jupyter.sh ; sleep 4 ; cat notebooks/jupyter.log ; echo 'setup : completed - jupyter token above';  sleep infinity"
+elif [[ $DOCKERLS != "lost_saturn" ]]; then
+   echo "setup ; option2"
+   docker run  -itd -p 9003:9003   --cap-add=NET_ADMIN --name lost_saturn jpacerqueira83/datascience-fullstack-vm1:latest
    echo "  wait 12 min  - lost_saturn - full setup in progress - STEP1"
    sleep 720
    ARGS1="nbconvert --to notebook --execute --allow-errors --ExecutePreporcessor.timeout=480 notebooks/$FILES "
