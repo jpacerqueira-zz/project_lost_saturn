9e98b4aefbda80ff017877841cfb795f65ff6e79
jpacerqueira <jpacerqueira@gmail.com>
2020-06-25 10:22:51 +0100

    automation 25June2020 - #COVID19

--- notebooks/covid19/daily-automation-notebook-21days.sh
+++ notebooks/covid19/daily-automation-notebook-21days.sh
@@ -9,16 +9,22 @@ $HOME/anaconda3/bin/jupyter \
        nbconvert --to notebook --execute --allow-errors --ExecutePreprocessor.timeout=1800 \
        $HOME/notebooks/covid19/MY_COVID19-Prediction_00MMYYYY-v14.ipynb  \
        --output $HOME/notebooks/covid19/MY_COVID19-Prediction_${DATENB}-14dayForecast-output-candidate.ipynb > crontab-run-$DATENB.log
+sleep 2
+rm -rf  /tmp/*
 # 21DayForecast
 $HOME/anaconda3/bin/jupyter \
        nbconvert --to notebook --execute --allow-errors --ExecutePreprocessor.timeout=1800 \
        $HOME/notebooks/covid19/MY_COVID19-Prediction_00MMYYYY.ipynb  \
        --output $HOME/notebooks/covid19/MY_COVID19-Prediction_${DATENB}-output-candidate.ipynb > crontab-run-$DATENB.log
+sleep 2
+rm -rf  /tmp/*
 # 42DayForecast
 $HOME/anaconda3/bin/jupyter \
        nbconvert --to notebook --execute --allow-errors --ExecutePreprocessor.timeout=1800 \
        $HOME/notebooks/covid19/MY_COVID19-Prediction_00MMYYYY-v42.ipynb  \
        --output $HOME/notebooks/covid19/MY_COVID19-Prediction_${DATENB}-42dayForecast-output-candidate.ipynb > crontab-run-$DATENB.log
+sleep 2
+rm -rf  /tmp/*
 # 63DayForecast
 $HOME/anaconda3/bin/jupyter \
        nbconvert --to notebook --execute --allow-errors --ExecutePreprocessor.timeout=1800 \
