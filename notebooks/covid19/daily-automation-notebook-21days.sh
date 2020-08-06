23a818a07f372ef6dc9b358135d7558d9e36beae
jpacerqueira <jpacerqueira@gmail.com>
2020-08-06 12:51:49 +0100

    Last review - more automation - 06Aug2020 - #COVID19

--- notebooks/covid19/daily-automation-notebook-21days.sh
+++ notebooks/covid19/daily-automation-notebook-21days.sh
@@ -10,30 +10,36 @@ $HOME/anaconda3/bin/jupyter \
        $HOME/notebooks/covid19/MY_COVID19-Prediction_00MMYYYY-v14.ipynb  \
        --output $HOME/notebooks/covid19/MY_COVID19-Prediction_${DATENB}-14dayForecast-output-candidate.ipynb > crontab-run-$DATENB.log
 mv $HOME/notebooks/covid19/heatmaps/Heatmap_Folium-Regional-${DATENB}-pred14Days.html $HOME/notebooks/covid19/heatmaps/Heatmap_Folium-Regional-${DATENB}-pred14Days-displayFuelBigData.html      
+bash -x $HOME/notebooks/covid19/heatmaps/sed-format-fit-website.sh $HOME/notebooks/covid19/heatmaps/Heatmap_Folium-Regional-${DATENB}-pred14Days-displayFuelBigData.html
 sleep 1
 rm -rf  /tmp/*
 # 21DayForecast
 $HOME/anaconda3/bin/jupyter \
        nbconvert --to notebook --execute --allow-errors --ExecutePreprocessor.timeout=1800 \
        $HOME/notebooks/covid19/MY_COVID19-Prediction_00MMYYYY.ipynb  \
-       --output $HOME/notebooks/covid19/MY_COVID19-Prediction_${DATENB}-output-candidate.ipynb > crontab-run-$DATENB.log
+       --output $HOME/notebooks/covid19/MY_COVID19-Prediction_${DATENB}-output-candidate.ipynb >> crontab-run-$DATENB.log
 mv $HOME/notebooks/covid19/heatmaps/Heatmap_Folium-Regional-${DATENB}-pred21Days.html $HOME/notebooks/covid19/heatmaps/Heatmap_Folium-Regional-${DATENB}-pred21Days-displayFuelBigData.html
+bash -x $HOME/notebooks/covid19/heatmaps/sed-format-fit-website.sh $HOME/notebooks/covid19/heatmaps/Heatmap_Folium-Regional-${DATENB}-pred21Days-displayFuelBigData.html
 sleep 1
 rm -rf  /tmp/*
 # 42DayForecast
 $HOME/anaconda3/bin/jupyter \
        nbconvert --to notebook --execute --allow-errors --ExecutePreprocessor.timeout=1800 \
        $HOME/notebooks/covid19/MY_COVID19-Prediction_00MMYYYY-v42.ipynb  \
-       --output $HOME/notebooks/covid19/MY_COVID19-Prediction_${DATENB}-42dayForecast-output-candidate.ipynb > crontab-run-$DATENB.log
+       --output $HOME/notebooks/covid19/MY_COVID19-Prediction_${DATENB}-42dayForecast-output-candidate.ipynb >> crontab-run-$DATENB.log
 mv $HOME/notebooks/covid19/heatmaps/Heatmap_Folium-Regional-${DATENB}-pred42Days.html $HOME/notebooks/covid19/heatmaps/Heatmap_Folium-Regional-${DATENB}-pred42Days-displayFuelBigData.html
+bash -x $HOME/notebooks/covid19/heatmaps/sed-format-fit-website.sh $HOME/notebooks/covid19/heatmaps/Heatmap_Folium-Regional-${DATENB}-pred42Days-displayFuelBigData.html
 sleep 1
 rm -rf  /tmp/*
 # 63DayForecast
 $HOME/anaconda3/bin/jupyter \
        nbconvert --to notebook --execute --allow-errors --ExecutePreprocessor.timeout=1800 \
        $HOME/notebooks/covid19/MY_COVID19-Prediction_00MMYYYY-v63.ipynb  \
-       --output $HOME/notebooks/covid19/MY_COVID19-Prediction_${DATENB}-63dayForecast-output-candidate.ipynb > crontab-run-$DATENB.log
+       --output $HOME/notebooks/covid19/MY_COVID19-Prediction_${DATENB}-63dayForecast-output-candidate.ipynb >> crontab-run-$DATENB.log
 mv $HOME/notebooks/covid19/heatmaps/Heatmap_Folium-Regional-${DATENB}-pred63Days.html $HOME/notebooks/covid19/heatmaps/Heatmap_Folium-Regional-${DATENB}-pred63Days-displayFuelBigData.html
+bash -x $HOME/notebooks/covid19/heatmaps/sed-format-fit-website.sh
+sleep 1
+rm -rf  /tmp/*
 #
 cp $HOME/notebooks/covid19/data/World\ v2\ --\ Confirmed\ -\ 63Day\ Forecast\ --\ train\ ${DATENB}.csv $HOME/notebooks/covid19/data/World\ v2\ --\ Confirmed\ -\ 1Day\ Forecast\ --\ train\ ${DATENB}.csv
 #
---
+++ notebooks/covid19/heatmaps/sed-format-fit-website.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+FILEA=$1
+#
+HOME=/home/notebookuser
+source $HOME/.profile
+cd $HOME/notebooks/covid19/heatmaps
+#
+sed -i 's/\n //g' $FILEA
+sed -i 's/\n //g' $FILEA
+sed -i 's/\n //g' $FILEA
+sed -i 's/\n //g' $FILEA
+#
+sed -i 's/   / /g' $FILEA
+sed -i 's/   / /g' $FILEA
+sed -i 's/   / /g' $FILEA
+sed -i 's/   / /g' $FILEA
+#
+sed -i 's/  / /g' $FILEA
+sed -i 's/  / /g' $FILEA
+sed -i 's/  / /g' $FILEA
+#
