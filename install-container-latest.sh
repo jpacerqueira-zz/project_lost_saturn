af88adb13183d072a7ed4a135cf9bd812136ebb2
jpacerqueira <jpacerqueira@gmail.com>
2020-10-11 23:03:59 +0100

    inprovements over the install-container setups

--- install-container-latest.sh
+++ install-container-latest.sh
@@ -20,8 +20,8 @@ FILES=Setup-System-Java-Spark-h2o.ai-pyarrow.ipynb # ipynb in test to load addic
 if [[ -f "$HERES/notebooks/$FILES" ]]; then
    ARGS1="nbconvert --to notebook --execute --allow-errors --ExecutePreporcessor.timeout=480 notebooks/$FILES "
    docker exec -it jupyter_container /bin/bash -c "cd ; source .bashrc ; bash -x ~/anaconda3/bin/jupyter $ARGS1 "
-   docker exec -it jupyter_container /bin/bash -c "cd ; source .bashrc ; bash -x start-jupyter.sh ; sleep 4 ; cat notebooks/jupyter.log ; sleep infinity"
+   docker exec -it jupyter_container /bin/bash -c "cd ; source .bashrc ; bash -x stop-jupyter.sh ; bash -x install-jupyter-support-packs.sh ; sleep 4 ; cat notebooks/jupyter.log ; sleep infinity"
 else
-   docker exec -it jupyter_container /bin/bash -c "cd ; source .bashrc ; bash -x start-jupyter.sh ; sleep 4 ; cat notebooks/jupyter.log ; sleep infinity"
+   docker exec -it jupyter_container /bin/bash -c "cd ; source .bashrc ; bash -x stop-jupyter.sh ; bash -x install-jupyter-support-packs.sh ; sleep 4 ; cat notebooks/jupyter.log ; sleep infinity"
 fi
 #
--- install-container.sh
+++ install-container.sh
@@ -9,7 +9,7 @@ echo "$DOCKERLS"
 DEST_FOLDER=/home/notebookuser/notebooks/
 #
 if [[ $DOCKERLS != "jupyter_container" ]]; then
-   docker run  -itd -p 9003:9003 --cap-add=NET_ADMIN --name jupyter_container --mount type=bind,source="$(pwd)"/notebooks,target=${DEST_FOLDER} jpacerqueira83/jupyter_datascience:latest
+   docker run  -itd -p 9003:9003 --cap-add=NET_ADMIN --name jupyter_container --mount type=bind,source="$(pwd)"/notebooks,target=${DEST_FOLDER} jpacerqueira83/jupyter_datascience:latest-stable-version-2.4.5
    echo "  wait 1 min  - jupyter_container - full installation/setup in progress "
    sleep 60
 fi
@@ -20,8 +20,8 @@ FILES=Setup-System-Java-Spark-h2o.ai-pyarrow.ipynb # ipynb in test to load addic
 if [[ -f "$HERES/notebooks/$FILES" ]]; then
    ARGS1="nbconvert --to notebook --execute --allow-errors --ExecutePreporcessor.timeout=480 notebooks/$FILES "
    docker exec -it jupyter_container /bin/bash -c "cd ; source .bashrc ; bash -x ~/anaconda3/bin/jupyter $ARGS1 "
-   docker exec -it jupyter_container /bin/bash -c "cd ; source .bashrc ; bash -x start-jupyter.sh ; sleep 4 ; cat notebooks/jupyter.log ; sleep infinity"
+   docker exec -it jupyter_container /bin/bash -c "cd ; source .bashrc ; bash -x stop-jupyter.sh ; bash -x install-jupyter-support-packs.sh ; sleep 4 ; cat notebooks/jupyter.log ; sleep infinity"
 else
-   docker exec -it jupyter_container /bin/bash -c "cd ; source .bashrc ; bash -x start-jupyter.sh ; sleep 4 ; cat notebooks/jupyter.log ; sleep infinity"
+   docker exec -it jupyter_container /bin/bash -c "cd ; source .bashrc ; bash -x stop-jupyter.sh ; bash -x install-jupyter-support-packs.sh ; sleep 4 ; cat notebooks/jupyter.log ; sleep infinity"
 fi
 #
