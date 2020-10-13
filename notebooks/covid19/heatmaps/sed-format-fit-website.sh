7f5cc3cf5c3e65e98324299ea4f42f6041365768
jpacerqueira <jpacerqueira@gmail.com>
2020-10-13 11:48:50 +0100

    New - run - #COVID19 - 13Oct2020

--- notebooks/covid19/heatmaps/sed-format-fit-website.sh
+++ notebooks/covid19/heatmaps/sed-format-fit-website.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 #
-FILEAN=$1
+#FILEAN=$1
 #
-HOMEY=/home/notebookuser
-source $HOMEY/.profile
-cd $HOMEY/notebooks/covid19/heatmaps
+#HOMEY=/home/notebookuser
+#source $HOMEY/.profile
+#cd $HOMEY/notebooks/covid19/heatmaps
 #
-#sed -i 's/\n //g' $FILEAN
-#sed -i 's/\n //g' $FILEAN
-#sed -i 's/\n //g' $FILEAN
-#sed -i 's/\n //g' $FILEAN
+#sed -i -e 's/\n //g' $FILEAN
+#sed -i -e 's/\n //g' $FILEAN
+#sed -i -e 's/\n //g' $FILEAN
+#sed -i -e 's/\n //g' $FILEAN
 #
-#sed -i 's/   / /g' $FILEAN
-#sed -i 's/   / /g' $FILEAN
-#sed -i 's/   / /g' $FILEAN
-#sed -i 's/   / /g' $FILEAN
+#sed -i -e 's/   / /g' $FILEAN
+#sed -i -e 's/   / /g' $FILEAN
+#sed -i -e 's/   / /g' $FILEAN
+#sed -i -e 's/   / /g' $FILEAN
 #
-#sed -i 's/  / /g' $FILEAN
-#sed -i 's/  / /g' $FILEAN
-#sed -i 's/  / /g' $FILEAN
+#sed -i -e 's/  / /g' $FILEAN
+#sed -i -e 's/  / /g' $FILEAN
+#sed -i -e 's/  / /g' $FILEAN
 #
