cdd38b02b159961fecb52909516d3d78760c3da9
jpacerqueira <jpacerqueira@gmail.com>
2020-10-30 11:19:02 +0000

    New - +daily spark reconciliation analysis  - 30Oct2020 - #COVID19

---
+++ notebooks/covid19/heatmaps/automate-daily-sed.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+MYDATE=$1
+#
+bash -x sed-format-fit-website-Copy2.sh Heatmap_Folium-Regional-${MYDATE}-pred14Days-displayFuelBigData.html
+bash -x sed-format-fit-website-Copy2.sh Heatmap_Folium-Regional-${MYDATE}-pred21Days-displayFuelBigData.html
+bash -x sed-format-fit-website-Copy2.sh Heatmap_Folium-Regional-${MYDATE}-pred42Days-displayFuelBigData.html
+bash -x sed-format-fit-website-Copy2.sh Heatmap_Folium-Regional-${MYDATE}-pred63Days-displayFuelBigData.html
+#
+bash -x sed-format-fit-website-Copy2.sh Heatmap_Folium-Global-${MYDATE}-pred14Days.html
+bash -x sed-format-fit-website-Copy2.sh Heatmap_Folium-Global-${MYDATE}-pred21Days.html
+bash -x sed-format-fit-website-Copy2.sh Heatmap_Folium-Global-${MYDATE}-pred42Days.html
+bash -x sed-format-fit-website-Copy2.sh Heatmap_Folium-Global-${MYDATE}-pred63Days.html
+#
+mv Heatmap_Folium-Global-${MYDATE}-pred14Days.html-e Heatmap_Folium-Global-YYYY-MM-DD-pred14Days.html 
+mv Heatmap_Folium-Global-${MYDATE}-pred21Days.html-e Heatmap_Folium-Global-YYYY-MM-DD-pred21Days.html 
+mv Heatmap_Folium-Global-${MYDATE}-pred42Days.html-e Heatmap_Folium-Global-YYYY-MM-DD-pred42Days.html 
+mv Heatmap_Folium-Global-${MYDATE}-pred63Days.html-e Heatmap_Folium-Global-YYYY-MM-DD-pred63Days.html 
+#
