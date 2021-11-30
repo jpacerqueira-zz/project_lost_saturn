d0e6b9be9d359a488729a3b785e42b42e41ebd77
jpacerqueira <jpacerqueira@gmail.com>
2021-11-30 10:39:58 +0000

    New - +daily spark reconciliation analysis  - 30Nov2021 - #COVID19

--- notebooks/covid19/daily-automation-notebook-21days.sh
+++ notebooks/covid19/daily-automation-notebook-21days.sh
@@ -119,7 +119,7 @@ sleep 1
 rm -rf /tmp/*
 #
 cd $HOME/notebooks/covid19/data/
-DROP_NDAYS=480
+DROP_NDAYS=495
 bash -x automate-daily-analysis-file.sh ${DATENB} ${DROP_NDAYS} >> $NBLOGFILE
 #Ω
 bash -x automate-daily-analysis-file.sh ${DATENB} ${DROP_NDAYS} >> $NBLOGFILE
